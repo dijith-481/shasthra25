6cb934f4749fffa39e814b5d6ad24c5ec407c823
dijith-481 <dijithdinesh@protonmail.com>
2025-06-18 11:05:49 +0530

    techparticles

---
+++ src/components/TechParticles.tsx
@@ -0,0 +1,53 @@
+import { useMemo, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial } from "@react-three/drei";
+
+export const TechParticles = () => {
+  return (
+    <Canvas className="" camera={{ position: [0, 0, 2] }}>
+      <Stars />
+    </Canvas>
+  );
+};
+
+function Stars(props: any) {
+  const ref = useRef<any>(null);
+  const [sphere] = useMemo(() => {
+    const count = 5000;
+    const positions = new Float32Array(count * 3);
+    for (let i = 0; i < count; i++) {
+      const r = 1.5 + Math.random() * 1.5;
+      const theta = Math.random() * 2 * Math.PI;
+      const phi = Math.acos(2 * Math.random() - 1);
+      positions[i * 3] = r * Math.sin(phi) * Math.cos(theta);
+      positions[i * 3 + 1] = r * Math.sin(phi) * Math.sin(theta);
+      positions[i * 3 + 2] = r * Math.cos(phi);
+    }
+    return [positions];
+  }, []);
+
+  useFrame((_state, delta) => {
+    ref.current.rotation.x -= delta / 20;
+    ref.current.rotation.y -= delta / 25;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
+        ref={ref}
+        positions={sphere}
+        stride={3}
+        frustumCulled={false}
+        {...props}
+      >
+        <PointMaterial
+          transparent
+          color="#20a985"
+          size={0.005}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
+}
